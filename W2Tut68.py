8dd079bb42b8f70f3fea477d42b969296891c7f9
Paul-Johan Seim <pjseim@gmail.com>
2015-07-18 17:46:13 -0400

    Update W2Tut68.py

--- W2Tut68.py
+++ W2Tut68.py
@@ -1,11 +1,13 @@
-    #Applying a list of functions to a variable x
+# Copyright Paul-Johan Seim
+
+# Applying a list of functions to a variable x
 def applyFuns(L, x):
     for f in L:
         print (f(x))
 
-    #applyFuns([abs, int], 4)
+# applyFuns([abs, int], 4)
 
-    #Map function
+# Map function
 S1=[1, -5, 20]
 print map(abs, S1)
 S2=[7, 2, 14]
