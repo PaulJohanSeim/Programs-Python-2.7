99bf4f7e23c2d4b4bd767d36c9e741f9b83339ce
Paul-Johan Seim <pjseim@gmail.com>
2015-07-18 17:27:39 -0400

    Update W2Tut33.py

--- W2Tut33.py
+++ W2Tut33.py
@@ -1,4 +1,8 @@
-                                #Bisection search Assignment*
+# Copyright Paul-Johan Seim
+
+# Bisection search Assignment
+
+
 print('Please think of a number between 0 and 100!')
 
 ans=0
