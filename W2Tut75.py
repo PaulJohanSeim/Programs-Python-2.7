70853b6ac78fda6251fe8f194df26c4b74c586af
Paul-Johan Seim <pjseim@gmail.com>
2015-07-18 17:49:47 -0400

    Update W2Tut75.py

--- W2Tut75.py
+++ W2Tut75.py
@@ -1,16 +1,17 @@
+# Copyright Paul-Johan Seim
+
 import math
     #Radiation exposure - radioactivity decay
 def radiationExposure(start, stop, step):
     '''
     Computes and returns the amount of radiation exposed
     to between the start and stop times. Calls the 
-    function f (defined for you in the grading script)
+    function f
     to obtain the value of the function at any point.
  
     start: integer, the time at which exposure begins
     stop: integer, the time at which exposure ends
-    step: float, the width of each rectangle. You can assume that
-      the step size will always partition the space evenly.
+    step: float, the width of each rectangle.
 
     returns: float, the amount of radiation exposed to 
       between start and stop times.
