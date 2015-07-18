6f13da72ccfbf2eaabcb6308a2f178f12a0cca1f
Paul-Johan Seim <pjseim@gmail.com>
2015-07-18 16:47:21 -0400

    Delete Quiz13.py

--- Quiz13.py
+++
@@ -1,9 +0,0 @@
-def laceStringsRecur(s1, s2):
-    def helpLaceStrings(s1, s2, out):
-        if s1 == '':
-            return s2
-        if s2 == '':
-            return s1
-        else:
-            return s1[0]+s2[0] + helpLaceStrings(s1[1:], s2[1:], out)
-    return helpLaceStrings(s1, s2, '')
