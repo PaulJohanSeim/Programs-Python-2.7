3e414f35f8da8610ed22045719569deccfbc2723
Paul-Johan Seim <pjseim@gmail.com>
2015-07-18 17:39:56 -0400

    Update W2Tut59.py

--- W2Tut59.py
+++ W2Tut59.py
@@ -1,10 +1,12 @@
-    #Recursive method of palindromes/semordnilap
+# Copyright Paul-Johan Seim
+
+# Recursive method of palindromes/semordnilap
 def semordnilapWrapper(str1, str2):
         # A single-length string cannot be semordnilap
         if len(str1) == 1 or len(str2) == 1:
             return False
 
-        # Equal strings cannot be semordnilap
+# Equal strings cannot be semordnilap
         if str1 == str2:
             return False
 
